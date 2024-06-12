74ade64341b5f2840bf73572f6138f6d48abc94d
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-12 13:09:39 +0200

    Set up my `useEffect` hook to fetch the blog post data

--- Asynchrony/src/App.jsx
+++ Asynchrony/src/App.jsx
@@ -5,6 +5,19 @@ export default function App() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch posts');
+        }
+        return response.json();
+      })
+      .then(data => setPosts(data))
+      .catch(error => setError(error.message));
+  }, []);
+
+
   return ()
 }
 
